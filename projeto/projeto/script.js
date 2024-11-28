ba04a95eacc91fe7e98cb6ea896e303d26cb221c
Elloar <elloa.123.t.nunes@gmail.com>
2024-11-28 15:00:14 -0300

    Add files via upload

---
+++ projeto/projeto/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+//     const email = document.getElementById("#emailNumero")
+//     const nomeusu = document.getElementById("#nomeUsuario")
+//     const senha = document.getElementById("#senhaCadastro")
+//     const senha2 = document.getElementById("#senhaCadastro2")
+
+
+//    function check() {
+//     const emailValor = email.value;
+//     const nomeusuValor = nomeusu.value;
+//     const senhaValor = senha.value;
+//     const senha2Valor = senha2.value;
+
+//     if(emailValor === ""){
+//         setErrorfor(email, "O nome de usuário é obrigatório.")
+//     }
+//    }
+
+//    function setErrorfor(input, menssage) {
+ 
+  
+//     function setErrorfor(input) {
+//         const formControl =  input.parentElement;
+
+//         formControl = "form-control success";
+//          }
+// }
